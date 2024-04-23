17c0460cdd16cf43e68ff1ea94b000de40574767
jorwi jesus <jgjg.0503@gmail.com>
2024-04-23 12:50:35 -0400

    update last

--- app/page.jsx
+++ app/page.jsx
@@ -8,6 +8,7 @@ import Pride from "react-canvas-confetti/dist/presets/pride";
 export default function Home() {
   const [isOpen,setIsOpen]=useState(0)
   const [isStarted,setIsStarted]=useState(false)
+  const [isFinished,setIsFinished]=useState(false)
   // const [count,setCount]=useState(1)
   const [width,setWidth]=useState(0)
   const [conductorC,setConductorC]=useState(0)
@@ -44,16 +45,32 @@ export default function Home() {
       if(count<16){
         setFoto(fotos[count].hre)
         count++
+        if(count==16){
+          setIsFinished(true)
+          setTimeout(()=>{
+            setIsOpen(0)
+            let count=0
+            setInterval(()=>{
+              if(count<20){
+                onShoot2()
+                count++
+              }
+            },50)
+          }, 15000)
+        }
       }
     },6000)
   }
 
   const onInitHandler = ({ conductor }) => {
-    setConductorC(conductor)
-    conductor.shoot();
     setTimeout(()=>{
+      setConductorC(conductor)
       conductor.shoot();
-    }, 1000)
+      setTimeout(()=>{
+        conductor.shoot();
+      }, 1000)
+    }, 600)
+    
   };
   const onInitHandler2 = ({ conductor }) => {
     setConductorC2(conductor)
@@ -70,13 +87,13 @@ export default function Home() {
 
   return (
     <main className="flex h-screen overflow-hidden flex-col items-center justify-between bg-rose-300">
-      <div className={"  drop-shadow-[4px_8px_1px_rgba(0,0,0,0.1)] relative flex h-full items-center"}>
+      <div className={"  drop-shadow-[4px_8px_1px_rgba(0,0,0,0.1)] relative flex h-full w-auto items-center "}>
         {/* {!isOpen&& */}
           <button onClick={()=>abrir(isOpen<2?isOpen+1:0)} className={(IsMoreWidth?' top-[420px] -right-14 ':(isOpen==2?' top-[70vh] right-0 ':' top-[70vh] -right-5 '))+" absolute  z-50 rotate-12  bg-rose-600 rounded-lg py-3 border-4 border-white px-6"}>{isOpen==0?'Abrir':isOpen==1?'Siguiente':'Cerrar'}</button>
         {/* } */}
         
-        <Realistic className="absolute bottom-0 z-40 w-[400px] h-[100vh]" onInit={onInitHandler} />
-        <Pride className="absolute bottom-0 z-40 w-[400px] h-[100vh]" onInit={onInitHandler2} />
+        <Realistic className="absolute bottom-0 z-40 w-full  h-[100vh]" onInit={onInitHandler} />
+        <Pride className="absolute bottom-0 z-40 w-full h-[100vh]" onInit={onInitHandler2} />
         {/* <button onClick={onShoot} className="absolute top-0 z-50">🎉</button> */}
         <Image
           src="/img/cumpleaños.jpg"
@@ -91,8 +108,10 @@ export default function Home() {
           <p className={(IsMoreWidth?' text-4xl p-20':' text-2xl p-10')}> Para la mujer mas hermosa de mi vida. <br/>Espero que hoy en tu día la pases muy bien.
           Te amo un monton..<br/> <strong>Mi Pinchura</strong>.</p>
         </div>
-        <div className={(isOpen==2?'pasar2':' pasar4 hidden')+(IsMoreWidth?' max-w-xl h-[100vh]':' w-[100vw] h-[100vh]') +' bg-white text-slate-800 p-8 text-justify flex-col justify-center items-center'}>
-          <p className="text-4xl p-5 px-20"><strong>Te Amo</strong>.</p>
+        <div className={(isOpen==2?'pasar2':' pasar4 hidden')+(IsMoreWidth?' max-w-xl h-[100vh]':' w-[100vw] h-[100vh]') +' bg-white min-w-96 text-slate-800 relative p-8 text-justify flex-col justify-center items-center'}>
+          <div className="absolute top-0 left-0 w-full h-screen flex justify-center items-center">
+            <p className={(isFinished?'aniWord':'hidden')+" text-xl p-5 px-20"}><strong>Te Amo</strong></p>
+          </div>
           {!isStarted?
             <div onClick={start} className="flex justify-center relative z-50 flex-col items-center bg-pink-500 cursor-pointer text-white p-4 py-6 rounded-full">
               <p className=" text-justify"><strong>Presiona</strong></p>
